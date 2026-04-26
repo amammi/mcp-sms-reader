eb21fd2e98fd1057250734327b776e63947af6e2
Andrea Mammì <ingandreamammi@gmail.com>
2026-04-26 17:25:46 +0200

    adds 2 new tools + fixed util read adb row function

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from fastmcp import Context, FastMCP
 from adb_pywrapper.adb_device import AdbDevice
 
 from utils import parse_adb_row
+from datetime import datetime
 
 
 mcp = FastMCP(name="MCP SMS Android reader", instructions="This server is used for reading sms from Android devices through ADB.")
@@ -12,6 +13,54 @@ async def list_devices():
     """Tool for finding all Android devices connected to PC"""
 
     return { "devices_connected": AdbDevice.list_devices() }
+
+
+@mcp.tool("read_all_sms")
+async def read_all_sms(device_name: str, ctx: Context):
+    """Given device_name in input as string, this tool returns all the sms stored in it."""
+
+    try:
+        device = AdbDevice(device=device_name)
+    except Exception:
+        return f"Error while trying to connect to device {device}. Is {device_name} connected or is the correct name?"
+    
+    result_command = device.shell("content query --uri content://sms/inbox")
+    rows = result_command.stdout.split("Row: ")[1::]
+    messages = {}
+
+    for i, row in enumerate(rows):
+        if len(row.strip()) > 0:
+            ctx.report_progress(i, len(rows), "Processing sms...")
+                
+            m = parse_adb_row(row)
+            messages[f"Message {i+1}"] = m 
+    
+    ctx.info(f"Read {len(messages)} messages.")
+    return messages  
+
+
+@mcp.tool("read_sms_between_dates")
+async def read_sms_between_dates(start: datetime, end: datetime, device_name: str, ctx: Context):
+    """Given start - end dates as datetime and device_name as string, this tool extracts all sms between this dates."""
+
+    try:
+        device = AdbDevice(device=device_name)
+    except Exception:
+        return f"Error while trying to connect to device {device}. Is {device_name} connected or is the correct name?"
+    
+    result_command = device.shell("content query --uri content://sms/inbox")
+    rows = result_command.stdout.split("Row: ")[1::]
+
+    messages = {}
+
+    for i, row in enumerate(rows):
+        if len(row.strip()) > 0:
+            m = parse_adb_row(row)
+            date = datetime.fromtimestamp(float(m['date']) / 1000 )
+            if start <= date <= end:
+                messages[f"Message {i+1}"] = m
+
+    return messages
 
 @mcp.tool("read_first_n_sms")
 async def read_first_n_sms(device_name: str, n: int, ctx: Context):
--- utils.py
+++ utils.py
@@ -2,10 +2,10 @@ import re
 
 def parse_adb_row(row):
     msg = {}
-    keys_positions = [(m.group(1), m.end()) 
+    keys_positions = [(m.group(1), m.start(), m.end()) 
                       for m in re.finditer(r'\b(\w+)=', row)]
     
-    for i, (key, end) in enumerate(keys_positions):
+    for i, (key, _, end) in enumerate(keys_positions):
         if i + 1 < len(keys_positions):
             value = row[end:keys_positions[i+1][1]].strip().rstrip(',').strip()
         else:
